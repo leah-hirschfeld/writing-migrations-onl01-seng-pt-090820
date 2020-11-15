71f843a271011771e8440e2b7d560f07b8b23250
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-15 20:45:46 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
     add_column :students,
-    t.integer :grade
+    :grade, :integer
     t.string :birthdate
   end
 end
